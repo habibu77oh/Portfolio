5e16013f25410d62b91f7ac30ef922f97c0fa507
habibu77oh <habibullohzekirullayev@gmail.com>
2024-11-17 18:00:34 +0500

    O'zgartirish qo'shish

--- src/pages/Home/Data.jsx
+++ src/pages/Home/Data.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Data() {
   return (
@@ -8,9 +9,9 @@ function Data() {
       <p className="home__description" style={{color:'#212121'}}>
         HTML ,CSS ,JS ,REACT ,TAILWIND ,node ,Git/GitHub BOOTSTRAP ,MUI ,THREE.JS
       </p>
-      <a href="#Contact" className="button button--flex" style={{backgroundColor: "black",}}>
+      <Link to={'/contact'} className="button button--flex" style={{backgroundColor: "black",}}>
         Say Hello
-      </a>
+      </Link>
     </div>
   );
 }
